87e3174fea2a26112a9163012180412b7dab636b
Randy <monyerandy15@gmail.com>
2023-10-15 23:44:55 +0100

    Template  / Project Setup

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-4">
+    <footer class="w-full box-border p-5 mt-3 text-center">
+      <p class="text-sm dark:text-gray-400">BookRite by Randy Monye</p>
+    </footer>
+  </div>
+</template>
